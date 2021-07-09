62a672664b0bc108b95b9a1f435032dd5971232e
Mo. Nadeem <mn.saeed91@gmail.com>
2021-07-09 16:48:29 +0530

    show people on index

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,13 +11,12 @@ class Person < ApplicationRecord
 	  header = spreadsheet.row(1)
 	  (2..spreadsheet.last_row).each do |i|
 	  row = Hash[[header, spreadsheet.row(i)].transpose]
-	  row = clean_for row, COLUMNS_TO_STRING
 	  person = Person.new
 	  person.attributes = row.to_hash.slice(*row.to_hash.keys)
 	  person.save!
 	 end
 	end
-	
+
 	def self.open_spreadsheet(file)
 	  case File.extname(file.original_filename)
 	  when ".csv" then Roo::CSV.new(file.path)
