1d136de3757bcc0934f907c3ec9d947f97374773
Mo. Nadeem <mn.saeed91@gmail.com>
2021-07-13 14:21:25 +0530

    added controller rspec

---
+++ spec/controllers/people_controller_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe PeopleController do
+  describe "POST #upload_csv" do
+    it "redirects to the home page" do
+      allow(People).to receive(:upload_csv).with("people.csv")
+      post :upload_csv, file: "people.csv"
+      expect(response).to redirect_to root_url
+    end
+
+    it "adds a flash notice" do
+      allow(People).to receive(:upload_csv).with("people.csv")
+      post :upload_csv, file: "people.csv"
+      expect(flash[:notice]).to eq "People imported successfully."
+    end
+
+    it "imports the people file" do
+      expect(People).to receive(:upload_csv).with("people.csv")
+      post :upload_csv, file: "people.csv"
+    end
+  end
+end
