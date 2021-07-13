52394de81555638f7f28dc4d6c7b19fd9a0a592d
Mo. Nadeem <mn.saeed91@gmail.com>
2021-07-13 14:05:05 +0530

    model rspec added

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,26 @@
+require 'rails_helper'
+
 RSpec.describe Person, :type => :model do
-  it "is valid with valid attributes"
-  it "is not valid without a first_name"
-  it "is not valid without a last_name"
+  describe "Person data validations" do
+    let(:person_1) { Person.new(first_name: 'first', last_name: 'last', email: 'test@example.com', phone: '+123456789') }
+
+    it "first_name and last_name is required" do
+      person_1.first_name = ''
+      person_1.last_name = ''
+      person_1.valid?
+      expect(person_1.errors.full_messages).to include("First name can't be blank", "Last name can't be blank")
+    end
+
+    it "email format validation" do
+      person_1.email = 'abc@com'
+      person_1.valid?
+      expect(person_1.errors.full_messages).to eq(["Email is invalid"])
+    end
+
+    it "phone format validation" do
+      person_1.phone = '1234A'
+      person_1.valid?
+      expect(person_1.errors.full_messages).to eq(["Phone is invalid"])
+    end
+  end
 end
